257f3b15bdef908ae3a971646f1f1b755fcd510f
Azeemi111 <817658@pdsb.net>
2025-03-03 10:50:32 -0500

    Initial Commit

---
+++ 05_CHALLANGE/05_DAY_OF_THE_WEEK_SUGGESTIONS.py
@@ -0,0 +1,21 @@
+print("This program will help you by providing activities to complete based on the day of the week!")
+day = input("Enter the day of the week: ")
+print()
+print()
+
+if day == "Monday" or day == "monday" or day =="MONDAY":
+    print("Start your week with a workout!")
+elif day == "Tuesday" or day == "tuesday" or day =="TUESDAY":
+    print("\033[34mIt's a great day to read a book!")
+elif day == "Wednesday" or day == "wednesday" or day =="WEDNESDAY":
+    print("\033[32mMid-week movie night!")
+elif day == "Thursday" or day == "thursday" or day =="THURSDAY":
+    print("\033[35mTry a new recipe!")
+elif day == "Friday" or day == "friday" or day =="FRIDAY":
+    print("\033[34mRelax and enjoy the weekend!")
+elif day == "Saturday" or day == "saturday" or day =="SATURDAY":
+    print("\033[35mGo for a hike!")
+elif day == "Sunday" or day == "sunday" or day =="SUNDAY":
+    print("\033[34mPrepare for the week ahead with some self-care.")
+else:
+    print("\033[31mPlease enter a valid day of the week! For example 'Sunday'.")
