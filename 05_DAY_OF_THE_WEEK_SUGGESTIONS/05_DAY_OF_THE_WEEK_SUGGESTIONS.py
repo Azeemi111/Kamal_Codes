d9baf7b93861f67094cc1a3b9a6ad6d7c2725c0b
Azeemi111 <817658@pdsb.net>
2025-03-04 22:04:42 -0500

    Initial Commit

--- 05_DAY_OF_THE_WEEK_SUGGESTIONS/05_DAY_OF_THE_WEEK_SUGGESTIONS.py
+++ 05_DAY_OF_THE_WEEK_SUGGESTIONS/05_DAY_OF_THE_WEEK_SUGGESTIONS.py
@@ -6,13 +6,13 @@
 # Updated:     4-Mar-2025
 # -----------------------------------------------------------------------------
 print("This program will help you by providing activities to complete based on the day of the week!")
-day = input("Enter the day of the week: ")
-print()
+day = input("Enter the day of the week: ") #Storeing users input by setting a variable
+print() #Spacing out the code to make it look visually appealing
 print()
 
-if day == "Monday" or day == "monday" or day =="MONDAY":
+if day == "Monday" or day == "monday" or day =="MONDAY": #Executing different possibilities based off the users input
     print("\033[34mStart your week with a workout!")
-elif day == "Tuesday" or day == "tuesday" or day =="TUESDAY":
+elif day == "Tuesday" or day == "tuesday" or day =="TUESDAY": #Keeping many "OR'S" incase the users spelling impacts the code
     print("\033[34mIt's a great day to read a book!")
 elif day == "Wednesday" or day == "wednesday" or day =="WEDNESDAY":
     print("\033[32mMid-week movie night!")
@@ -24,5 +24,5 @@ elif day == "Saturday" or day == "saturday" or day =="SATURDAY":
     print("\033[35mGo for a hike!")
 elif day == "Sunday" or day == "sunday" or day =="SUNDAY":
     print("\033[34mPrepare for the week ahead with some self-care.")
-else:
+else: #Setting a response if the user inputs a number different then the possibilities listed above
     print("\033[31mPlease enter a valid day of the week! For example 'Sunday'.")
